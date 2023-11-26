cfb14095e57069bbb44dafe3b2ee28d8a630c3f8
Eclipse <152074226+zEclypsium@users.noreply.github.com>
2023-11-26 13:36:01 +0000

    Updated sspm.js
    Optimized sub-optimal code ; added download animation

--- sspm.js
+++ sspm.js
@@ -1,167 +1,148 @@
 const axios = require('axios');
 const readline = require('readline');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const { program } = require('commander');
 
 const CONFIG_FOLDER = './config';
 const CONFIG_FILE = path.join(CONFIG_FOLDER, 'config.json');
 const BASE_URL = 'https://api.github.com/repos/MucciDev/SSPM/contents/libraries';
+const spinner = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-function saveConfig(configData) {
-    if (!fs.existsSync(CONFIG_FOLDER)) {
-        fs.mkdirSync(CONFIG_FOLDER);
+async function saveConfig(configData) {
+    try {
+        await fs.mkdir(CONFIG_FOLDER, { recursive: true });
+        await fs.writeFile(CONFIG_FILE, JSON.stringify(configData));
+    } catch (err) {
+        console.error(err);
     }
-    fs.writeFileSync(CONFIG_FILE, JSON.stringify(configData));
 }
 
-function loadConfig() {
+async function loadConfig() {
     try {
-        const data = fs.readFileSync(CONFIG_FILE, 'utf8');
+        const data = await fs.readFile(CONFIG_FILE, 'utf8');
         return JSON.parse(data);
     } catch (err) {
         return {};
     }
 }
 
-function createLibrariesFolder(librariesPath) {
-    const librariesFolderPath = path.join(librariesPath, 'libraries');
-    if (!fs.existsSync(librariesFolderPath)) {
-        fs.mkdirSync(librariesFolderPath);
-    }
-    return librariesFolderPath;
-}
-
-function downloadFileFromGitHub(repoOwner, repoName, filePath) {
+async function downloadFileFromGitHub(repoOwner, repoName, filePath) {
     const url = `https://raw.githubusercontent.com/${repoOwner}/${repoName}/main/${filePath}`;
-    return axios.get(url);
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        throw new Error(`Failed to download file from GitHub: ${error.message}`);
+    }
 }
 
-function downloadLibrary(libraryName, librariesPath) {
-    return new Promise((resolve, reject) => {
+async function downloadLibrary(libraryName, librariesPath) {
+    let spinnerInterval;
+    try {
         const libraryFolderPath = path.join(librariesPath, 'libraries', libraryName);
-        if (!fs.existsSync(libraryFolderPath)) {
-            fs.mkdirSync(libraryFolderPath, { recursive: true });
-        }
+        await fs.mkdir(libraryFolderPath, { recursive: true });
 
         const libraryPath = `libraries/${libraryName}`;
-        axios.get(BASE_URL)
-            .then(response => {
-                const libraries = response.data.filter(item => item.path === libraryPath);
+        const response = await axios.get(BASE_URL);
+        const libraries = response.data.filter(item => item.path === libraryPath);
 
-                if (libraries.length > 0) {
-                    const library = libraries[0];
-                    return axios.get(library.url);
+        if (libraries.length > 0) {
+            const library = libraries[0];
+            const libraryData = await axios.get(library.url);
+            const libraryFiles = libraryData.data.filter(item => item.name.endsWith('.spwn'));
+
+            if (libraryFiles.length > 0) {
+                let i = 0;
+                spinnerInterval = setInterval(() => {
+                    process.stdout.write(`\rDownloading ${spinner[i % spinner.length]} ${libraryName}...`);
+                    i++;
+                }, 100);
+
+                await Promise.all(libraryFiles.map(async file => {
+                    const fileData = await downloadFileFromGitHub('MucciDev', 'SSPM', `${libraryPath}/${file.name}`);
+                    const filePath = path.join(libraryFolderPath, file.name);
+                    await fs.writeFile(filePath, fileData);
+                    console.log(`\rFile '${file.name}' downloaded and saved at: ${filePath}`);
+                }));
+
+                clearInterval(spinnerInterval);
+                process.stdout.write('\r'); // Clear the spinner animation
+            } else {
+                throw new Error(`No SPWN files found in the '${libraryName}' library.`);
+            }
+        } else {
+            throw new Error(`Library '${libraryName}' not found.`);
+        }
+    } catch (error) {
+        console.error(error.message);
+        clearInterval(spinnerInterval);
+        process.stdout.write('\r'); // Clear the spinner animation on error
+    }
+}
+
+async function start() {
+    try {
+        const config = await loadConfig();
+        let librariesPath = config.lastPath || '';
+
+        try {
+            await fs.access(librariesPath);
+        } catch (err) {
+            librariesPath = '';
+        }
+
+        if (librariesPath) {
+            rl.question(`Use the last used path '${librariesPath}'? (y/n): `, async (answer) => {
+                if (answer.toLowerCase() === 'y') {
+                    await startLibraryImport(librariesPath);
                 } else {
-                    throw new Error(`Library '${libraryName}' not found.`);
+                    await getNewLibraryPath();
                 }
-            })
-            .then(response => {
-                const libraryFiles = response.data.filter(item => item.name.endsWith('.spwn'));
-
-                if (libraryFiles.length > 0) {
-                    const downloadPromises = libraryFiles.map(file => {
-                        const fileURL = file.download_url;
-                        const fileName = file.name;
-                        return downloadFileFromGitHub('MucciDev', 'SSPM', `${libraryPath}/${fileName}`)
-                            .then(fileData => {
-                                const filePath = path.join(libraryFolderPath, fileName);
-                                fs.writeFileSync(filePath, fileData.data);
-                                console.log(`File '${fileName}' downloaded and saved at: ${filePath}`);
-                            });
-                    });
-
-                    Promise.all(downloadPromises)
-                        .then(() => resolve())
-                        .catch(() => reject());
-                } else {
-                    throw new Error(`No SPWN files found in the '${libraryName}' library.`);
-                }
-            })
-            .catch(error => {
-                console.error(error.message);
-                reject();
             });
-    });
-}
-
-program
-    .option('-s, --set-path <path>', 'Set path for libraries folder')
-    .parse(process.argv);
-
-let librariesPath = '';
-
-// Check if a path is provided via command line
-if (program.setPath) {
-    librariesPath = path.resolve(program.setPath);
-
-    if (!fs.existsSync(librariesPath)) {
-        console.error('Specified path does not exist.');
-        process.exit(1);
-    }
-} else {
-    const config = loadConfig();
-    librariesPath = config.lastPath || '';
-
-    if (!fs.existsSync(librariesPath)) {
-        librariesPath = '';
+        } else {
+            await getNewLibraryPath();
+        }
+    } catch (error) {
+        console.error(error.message);
     }
 }
 
-if (librariesPath) {
-    rl.question(`Use the last used path '${librariesPath}'? (y/n): `, (answer) => {
-        if (answer.toLowerCase() === 'y') {
-            startLibraryImport(librariesPath);
-        } else {
-            getNewLibraryPath();
-        }
-    });
-} else {
-    getNewLibraryPath();
-}
+async function startLibraryImport(librariesPath) {
+    try {
+        rl.question('Enter command: ', async (command) => {
+            const args = command.split(' ').slice(2);
 
-function getNewLibraryPath() {
-    rl.question('Enter path for libraries folder: ', (inputPath) => {
-        librariesPath = path.resolve(inputPath);
+            if (command.startsWith('sspm import') && args.length > 0) {
+                await downloadLibraries(args, librariesPath);
+            } else {
+                console.log('Invalid command!');
+            }
 
-        if (!fs.existsSync(librariesPath)) {
-            console.error('Specified path does not exist.');
-            process.exit(1);
-        }
-
-        startLibraryImport(librariesPath);
-    });
-}
-
-function startLibraryImport(librariesPath) {
-    rl.question('Enter command: ', (command) => {
-        const args = command.split(' ').slice(2);
-
-        if (command.startsWith('sspm import') && args.length > 0) {
-            downloadLibraries(args, librariesPath);
-        } else {
-            console.log('Invalid command!');
-        }
-
-        rl.close();
-    });
-}
-
-function downloadLibraries(libraryNames, librariesPath) {
-    const downloadPromises = libraryNames.map(libraryName => {
-        return downloadLibrary(libraryName, librariesPath);
-    });
-
-    Promise.all(downloadPromises)
-        .then(() => {
-            console.log('All libraries downloaded successfully.');
-        })
-        .catch(() => {
-            console.error('Failed to download one or more libraries.');
+            rl.close();
         });
+    } catch (error) {
+        console.error(error.message);
+    }
 }
+
+async function downloadLibraries(libraryNames, librariesPath) {
+    try {
+        const downloadPromises = libraryNames.map(libraryName => {
+            return downloadLibrary(libraryName, librariesPath);
+        });
+
+        await Promise.all(downloadPromises);
+        console.log('All libraries downloaded successfully.');
+    } catch (error) {
+        console.error('Failed to download one or more libraries.');
+    }
+}
+
+// Start the program
+start();
